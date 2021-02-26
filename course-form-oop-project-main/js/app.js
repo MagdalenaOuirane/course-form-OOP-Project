34ffda1237c40e696a84f7c5cfd9f182d5b2255b
Magdalena <magdalenaouirane@gmail.com>
2021-02-26 13:23:41 +0100

    add clearInputs functionality

--- course-form-oop-project-main/js/app.js
+++ course-form-oop-project-main/js/app.js
@@ -8,7 +8,7 @@ class Customer {
   }
 }
 
-// Display class : handle UI tasks
+// Display class : handle card tasks
 
 class Display {
   constructor() {
@@ -27,7 +27,7 @@ class Display {
     console.log(div);
     div.classList.add("col-11", "mx-auto", "col-md-6", "my-3", "col-lg-4");
     div.innerHTML = `<div class="card text-left">
-    img src="./img/cust-${random}.jpg" class="card-img-top" alt="">
+   <img src="./img/cust-${random}.jpg" class="card-img-top" alt="">
          <div class="card-body">
           <!-- customer name -->
           <h6 class="text-capitalize "><span class="badge badge-warning mr-2">name : ${customer.customerName}</span><span id="customer-name"></span></h6>
@@ -49,26 +49,32 @@ class Display {
     let random = Math.floor(Math.random() * 5 + 1);
     return random;
   }
+
+  clearInputs() {
+    this.name.value = "";
+    this.course.value = "";
+    this.author.value = "";
+  }
 }
 
-// event add customer on submit
+// event add customer class object on submit button
 
 document.querySelector("#customer-form").addEventListener("submit", (e) => {
   e.preventDefault();
 
   //get Form values
-  const customerName = document.querySelector(".name").value;
-  const courseForm = document.querySelector(".course").value;
-  const authorForm = document.querySelector(".author").value;
+  const nameValue = document.querySelector(".name").value;
+  const courseValue = document.querySelector(".course").value;
+  const authorValue = document.querySelector(".author").value;
 
-  // Instatiate course object
-
-  const customer = new Customer(customerName, courseForm, authorForm);
+  // Instatiate customer class object
+  const customer = new Customer(nameValue, courseValue, authorValue);
 
   console.log(customer);
 
-  // Add
-  const ui = new Display();
+  // Instatiate Display class object
+  const card = new Display();
 
-  ui.addCustomersToList(customer);
+  card.addCustomersToList(customer);
+  card.clearInputs();
 });
